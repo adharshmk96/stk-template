760682a3fdefed669535840cf0ef09b5a02d6283
Adharsh Manikandan <adharsh.manikandan@agoda.com>
2023-10-10 17:53:48 +0700

    updated templategen.

--- singlemod/template-gen.py
+++ singlemod/template-gen.py
@@ -15,6 +15,12 @@ def replace_pkg_app_names(content):
     content = content.replace('github.com/adharshmk96/stk-template/singlemod', '{{ .PkgName }}')
     return content
 
+def replace_module_names(content):
+    # Replacing specific strings with given replacements
+    content = content.replace('ping', '{{ .ModName }}')
+    content = content.replace('Ping', '{{ .ExportedName }}')
+    return content
+
 def ignore_dir(dir):
     if ".git" in dir:
         return True
@@ -23,7 +29,7 @@ def generate_var_name(file_path, base_path):
     relative_path = file_path.replace(base_path, '').replace('\\', '/')
     var_name = relative_path.replace(os.sep, '_').replace('.', '').strip('_').replace('-', '').replace('/', '')
     var_name = var_name.upper()
-    return relative_path, var_name + "_TPL"
+    return relative_path, var_name
 
 def find_project_template_map(base_path):
     project_template_map = []
@@ -34,7 +40,7 @@ def find_project_template_map(base_path):
                    
             absolute_path = os.path.join(root, f)
             relative_path, var_name = generate_var_name(absolute_path, base_path)
-            project_template_map.append(TemplateMap(absolute_path, relative_path, var_name))
+            project_template_map.append(TemplateMap(absolute_path, relative_path, var_name + "_TPL"))
     return project_template_map
 
 def generate_project_template(base_path):
@@ -61,9 +67,51 @@ def create_project_template(base_directory, target_file_path):
     project_template = generate_project_template(base_directory)
     write_project_template_to_file(target_file_path, project_template, project_template_map)
 
+
+def find_module_template_map(base_path):
+    module_template_map = []
+    for root, _, files in os.walk(base_path):
+        for f in files:
+            if ("ping.go" not in f) or ignore_dir(root) or (f in ignore_files):
+                continue
+
+            absolute_path = os.path.join(root, f)
+            relative_path, var_name = generate_var_name(absolute_path, base_path)
+            module_template_map.append(TemplateMap(absolute_path, relative_path, var_name+"_MOD"))
+    return module_template_map
+
+def generate_module_template(base_path):
+    module_template_map = find_module_template_map(base_path)
+    module_template = "package tpl\n\n"
+    for template in module_template_map:
+        with open(template.absolute_path, 'r', encoding='utf-8') as input_file:
+            content = input_file.read()
+            content = replace_pkg_app_names(content)
+            content = replace_module_names(content)
+
+            file_path = replace_module_names(template.relative_path)
+
+            structure = f'var {template.var_name} = Template{{\n\tFilePath: "{file_path}",\n\tContent: `{content}`,\n}}\n\n'
+            module_template += structure
+    return module_template
+
+def write_module_template_to_file(target_path, file_content, module_template_map):
+    with open(target_path, 'w') as output_file:
+        output_file.write(file_content)
+        output_file.write("var ModuleTemplates = []Template{\n")
+        for template in module_template_map:
+            output_file.write(f'\t{template.var_name},\n')
+        output_file.write("}\n")
+
+def create_module_template(base_directory, target_file_path):
+    module_template_map = find_module_template_map(base_directory)
+    module_template = generate_module_template(base_directory)
+    write_module_template_to_file(target_file_path, module_template, module_template_map)
+
 if __name__ == '__main__':
     base_directory = "./"
-    target_file_path = 'singlemod.go'
-    create_project_template(base_directory, target_file_path)
-
-    print(f"All files have been written to {target_file_path}.")
+    project_template_path = 'singlemod.go'
+    module_template_path = 'ping.go'
+    create_project_template(base_directory, project_template_path)
+    create_module_template(base_directory, module_template_path)
+    print(f"All files have been written to {project_template_path}, {module_template_path}.")
